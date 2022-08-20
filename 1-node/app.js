010e68651679d728aa8c49506abc9edbc3c408c2
Teresia-Kirungo <terrykirungo@gmail.com>
2022-08-20 23:22:15 +0300

    Fix

--- 1-node/app.js
+++ 1-node/app.js
@@ -1,10 +1,15 @@
-// GLOBALS - NO WINDOW !!!! because there's no console or DOM
+/* 
+GLOBALS - NO WINDOW !!!! because there's no console or DOM
+Global variables can be accessed from anywhere
 
-// __dirname - path to current directory(file path)
-// __filename - file name
-// require - function to use modules(files in JS(CommonJS)are also called modules)
-// module - info about current module(file)
-// process - info about env where the program is executed
+ __dirname - path to current directory(file path)
+ __filename - file name
+ require - function to use modules(files in JS(CommonJS)are also called modules)
+ module - info about current module(file)
+ process - info about env where the program is executed
+
+
+*/
 
 //this gives you the path to where this file is
 console.log(__dirname)
